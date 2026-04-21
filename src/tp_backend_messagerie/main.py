2fb09a5c2af02f531e9002359437e42e920ea263
Raphaëlle Christmann <raphaelle.christmann@etu.minesparis.psl.eu>
2026-04-21 13:30:28 +0200

    premier jet de main.py et premier lancement de l'app

---
+++ src/tp_backend_messagerie/main.py
@@ -0,0 +1,19 @@
+"""
+Module qui gère :
+- la création de l'application FastAPI
+- l'initialisation des tables au démarrage via l'appel à init_db()
+- branchement des routers
+"""
+
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from tp_backend_messagerie.database import init_db
+from tp_backend_messagerie.models import User, Message
+
+@asynccontextmanager
+async def lifespan(app : FastAPI):
+    init_db() 
+    yield
+
+app = FastAPI(lifespan = lifespan)
+
